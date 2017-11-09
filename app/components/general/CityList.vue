ca638f36b26474eb8d7438ee9813a00ce578d6c8
artur.rachwal <artur.rachwal@gmail.com>
2017-11-09 13:24:24 +0100

    html improved

--- app/components/general/CityList.vue
+++ app/components/general/CityList.vue
@@ -3,7 +3,7 @@
 <template>
   <div class="container-fluid">
     <!-- !!!!! 3 means column count here!!!!! -->
-    <div class="row top-buffer-md" v-for="(number, index) in Math.floor(cities.length / 3) + (cities.length / 3 % 1 == 0 ? 0 : 1)" :key="index" >
+    <div class="row top-buffer-md" v-for="(number, index) in Math.floor(cities.length / 3) + Number((cities.length / 3) % 1 != 0)" :key="number" >
         <div class="col-xs-4 inner-buffer-md" v-for="city in cities.slice(3 * index, 3 * index + 3)" :key="city.name">
             <div class="tile">
                 {{ city.name }}
